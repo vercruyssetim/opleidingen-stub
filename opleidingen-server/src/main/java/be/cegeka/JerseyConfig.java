d5ce02967152b2b737dd24d509b053a42f310caa
vercruyssetim <tim.vercruysse@cegeka.be>
2017-05-03 17:19:26 +0200

    [Tim] - initial commit for ci

--- opleidingen-server/src/main/java/be/cegeka/JerseyConfig.java
+++ opleidingen-server/src/main/java/be/cegeka/JerseyConfig.java
@@ -1,5 +1,6 @@
 package be.cegeka;
 
+import be.cegeka.helloWorld.HelloWorldResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ import javax.ws.rs.ApplicationPath;
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        packages(getClass().getPackage().toString());
+        registerClasses(HelloWorldResource.class);
         property(ServerProperties.WADL_FEATURE_DISABLE, true);
     }
 }
