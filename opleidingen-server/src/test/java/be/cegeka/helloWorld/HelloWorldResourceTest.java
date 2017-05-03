0b2bdffe396435160bcafd5d5d842de6c9793208
vercruyssetim <tim.vercruysse@cegeka.be>
2017-05-03 15:31:57 +0200

    [Tim] - initial commit for ci

---
+++ opleidingen-server/src/test/java/be/cegeka/helloWorld/HelloWorldResourceTest.java
@@ -0,0 +1,12 @@
+package be.cegeka.helloWorld;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class HelloWorldResourceTest {
+
+    @Test
+    public void test() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
